3063ff1a18234bd7517b4035bb374474571e5e63
Fayas <fayasahamed632@gmail.com>
2026-03-10 21:16:36 +0530

    Implemented custom fixtures for page objects

--- tests/ui/purchase.spec.ts
+++ tests/ui/purchase.spec.ts
@@ -1,43 +1,38 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '../../fixtures/baseTest'
 
-import { LoginPage } from '../../pages/LoginPage'
-import { InventoryPage } from '../../pages/InventoryPage'
-import { CartPage } from '../../pages/CartPage'
-import { CheckoutPage } from '../../pages/CheckoutPage'
+test('complete purchase flow', async ({
 
-test('complete purchase flow', async ({ page }) => {
+ loginPage,
+ inventoryPage,
+ cartPage,
+ checkoutPage,
+ page
 
-  const loginPage = new LoginPage(page)
-  const inventoryPage = new InventoryPage(page)
-  const cartPage = new CartPage(page)
-  const checkoutPage = new CheckoutPage(page)
+}) => {
 
-  await loginPage.navigateToLoginPage()
+ await loginPage.navigateToLoginPage()
 
-  await loginPage.login(
-    'standard_user',
-    'secret_sauce'
-  )
+ await loginPage.login('standard_user','secret_sauce')
 
-  await inventoryPage.verifyInventoryPage()
+ await inventoryPage.verifyInventoryPage()
 
-  await inventoryPage.addBackpackToCart()
+ await inventoryPage.addBackpackToCart()
 
-  await inventoryPage.openCart()
+ await inventoryPage.openCart()
 
-  await cartPage.clickCheckout()
+ await cartPage.clickCheckout()
 
-  await checkoutPage.enterCheckoutDetails(
-    'John',
-    'Doe',
-    '600001'
-  )
+ await checkoutPage.enterCheckoutDetails(
+   'John',
+   'Doe',
+   '600001'
+ )
 
-  await checkoutPage.continueCheckout()
+ await checkoutPage.continueCheckout()
 
-  await checkoutPage.finishCheckout()
+ await checkoutPage.finishCheckout()
 
-  await expect(page.locator('.complete-header'))
-    .toHaveText('Thank you for your order!')
+ await expect(page.locator('.complete-header'))
+   .toHaveText('Thank you for your order!')
 
 })
